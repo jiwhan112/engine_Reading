5144a0dc072b5670dc1e94ecff1bae2203e0c9ec
김덕배 <jiwhan112@naver.com>
2017-03-07 19:10:30 +0900

    엔진실습
    엔진실습_3월 7일 수업 버텍스 트라이앵글, 정점 쉐이더, 리깅, 애니메이션 유니티 코드로 짠 것

---
+++ Assets/CreateMesh.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateMesh : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        float p = 1.0f;
+        Mesh m = new Mesh();
+        m.vertices = new Vector3[]
+        {
+            new Vector3(-5.0f,-5.0f,0.0f),
+            new Vector3(-5.0f,5.0f,0.0f),
+            new Vector3(5.0f,5.0f,0.0f),
+
+            new Vector3(5.0f,-5.0f,0.0f)
+        };
+        //버텍스 정보로 삼각형을 만든다.
+        m.triangles = new int[] { 0,1,2,0,2,3};// 언제나 3의 배수여야 한다.
+        m.uv = new Vector2[]// 텍스처의 UV를 펴주자
+        {
+            new Vector2(0.0f,0.0f),
+            new Vector2(0.0f,p),
+            new Vector2(p,p),
+            new Vector2(p,0.0f)
+        };
+        //버텍스 컬러
+        m.colors = new Color[]
+        {
+          Color.red,
+          Color.black,
+          Color.blue,
+          Color.gray
+        };  
+
+
+        GetComponent<MeshFilter>().mesh = m;
+       
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+}
---
+++ Assets/CustomSkinnedMesh.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomSkinnedMesh : MonoBehaviour {
+    // 코드로 리깅 연습
+    public Material mat;
+    public Transform[] bones;
+	// Use this for initialization
+	void Start ()
+    {
+        Mesh m = new Mesh();
+
+
+        m.vertices = new Vector3[]
+     {
+               new Vector3(0.0f,0.0f,0.0f),
+               new Vector3(-5.0f,5.0f,0.0f),
+               new Vector3(5.0f,5.0f,0.0f)
+     };
+
+        m.triangles = new int[] { 0, 1, 2};// 언제나 3의 배수여야 한다.
+        
+       
+        m.bindposes = new Matrix4x4[]
+        {
+            bones[0].worldToLocalMatrix*transform.localToWorldMatrix,
+            bones[1].worldToLocalMatrix*transform.localToWorldMatrix
+        };//위치를 잡아줌
+
+        //버텍스와 웨이트를 칠함
+        m.boneWeights = new BoneWeight[]
+        {
+             new BoneWeight() { boneIndex0=0,weight0=1f },//묶여있는 본 
+             new BoneWeight() {boneIndex0=1,weight1=1f },
+             new BoneWeight() {boneIndex0=0,weight1=1f }
+        };
+        //우리가 만든 매쉬가 들어감
+        SkinnedMeshRenderer smr = GetComponent<SkinnedMeshRenderer>();
+        smr.sharedMesh = m;
+        smr.material = mat;
+        smr.bones = bones;
+        smr.rootBone = bones[0];
+        smr.quality = SkinQuality.Bone1;
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
